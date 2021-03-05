0ca618f651154d5be6e5adcb412993c6d05a9177
himanmenGit <study.himanmen@gmail.com>
2021-03-05 19:03:55 +0900

    config 분리

--- cookbook/config/settings.py
+++ cookbook/config/settings.py
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'cookbook.urls'
+ROOT_URLCONF = 'cookbook.config.urls'
 
 TEMPLATES = [
     {
@@ -73,7 +73,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'cookbook.wsgi.application'
+WSGI_APPLICATION = 'cookbook.config.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
